265ffc8e2dea485c8268bd653c5c85533a104b4f
KimKunG <kimkung.ch.mc@gmail.com>
2023-01-25 19:09:02 +0700

    Add Register Config

--- src/main/java/net/ppekkungz/Command/KmTabCommand.java
+++ src/main/java/net/ppekkungz/Command/KmTabCommand.java
@@ -9,25 +9,29 @@ import net.ppekkungz.KmTab;
 import java.util.ArrayList;
 import java.util.List;
 
-public class KmTabCommand implements TabExecutor {
+public class KmTabCommand extends Command implements TabExecutor {
     private final KmTab kmcore;
 
     public KmTabCommand(KmTab kmcore) {
-    this.kmcore = kmcore;
+        super("kmtab");
+        this.kmcore = kmcore;
     }
-
-    public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
-        if (args.length >= 1 &&
-        args[0].equals("reload")) {
-            sender.sendMessage("" + ChatColor.GREEN + "KmTab-WaterFall : ReloadConfig!!");
+    @Override
+    public void execute(CommandSender sender, String[] args) {
+        if (args.length >= 1 && args[0].equals("reload")) {
+            sender.sendMessage(ChatColor.GREEN + "KmTab-WaterFall : ReloadConfig!!");
             this.kmcore.reloadConfig();
         }
-        return false;
     }
+
+    @Override
     public List<String> onTabComplete(CommandSender sender, String[] args) {
         ArrayList<String> lists = new ArrayList<>();
-        if (args.length == 1)
+        if (args.length == 1) {
             lists.add("reload");
+        }
         return lists;
     }
+
+
 }
--- src/main/java/net/ppekkungz/KmTab.java
+++ src/main/java/net/ppekkungz/KmTab.java
@@ -1,19 +1,27 @@
 package net.ppekkungz;
 
-import net.md_5.bungee.api.plugin.Listener;
+import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.plugin.Plugin;
+import net.md_5.bungee.config.Configuration;
+import net.md_5.bungee.config.ConfigurationProvider;
+import net.md_5.bungee.config.YamlConfiguration;
 import net.ppekkungz.Command.KmTabCommand;
 
-
-import java.util.ArrayList;
-import java.util.Objects;
+import java.io.File;
+import java.io.FileWriter;
 
 public class KmTab extends Plugin {
+
+    public Configuration configuration;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
         System.out.println("KmTab-Waterfall : Start Testing....");
+        // Config
+        makeConfig();
+        // RegisterCommand
+        ProxyServer.getInstance().getPluginManager().registerCommand(this, new KmTabCommand(this));
     }
 
     @Override
@@ -22,6 +30,31 @@ public class KmTab extends Plugin {
         System.out.println("KmTab-Waterfall : Stop Testing....");
     }
 
-    public void reloadConfig() {
+    private void makeConfig() {
+        try {
+            if (!getDataFolder().exists()) {
+                getLogger().info("Created config folder: " + getDataFolder().mkdir());
+            }
+            File configFile = new File(getDataFolder(), "config.yml");
+            if (!configFile.exists()) {
+                FileWriter fileWriter = new FileWriter(configFile);
+                fileWriter.write("ListHeader:\n-TEST\nListFooter:\n-TEST");
+                fileWriter.close();
+            }
+            configuration = ConfigurationProvider.getProvider(YamlConfiguration.class).load(configFile);
+        } catch (Exception ignored) {
+            getLogger().warning("Can't Load Config!");
+        }
     }
+
+    public void reloadConfig() {
+        try {
+            File configFile = new File(getDataFolder(), "config.yml");
+            configuration = ConfigurationProvider.getProvider(YamlConfiguration.class).load(configFile);
+            getLogger().info("Reload Config!");
+        } catch (Exception ignored) {
+            getLogger().warning("Can't Reload Config!");
+        }
+    }
+
 }
